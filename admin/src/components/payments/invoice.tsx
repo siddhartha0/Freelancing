aedb1bb64ff34705ec6a25cf0bc224600ae026f3
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-03 11:03:59 +0545

    init

--- admin/src/components/payments/invoice.tsx
+++ admin/src/components/payments/invoice.tsx
@@ -1,5 +1,5 @@
 import { useGetProjectByIdQuery } from "@/state-management";
-import { LoaderSpinner } from "@/units";
+import { LoaderSpinner, Text } from "@/units";
 import { useParams } from "react-router-dom";
 
 export const Invoice = () => {
@@ -7,11 +7,48 @@ export const Invoice = () => {
 
   const { data, isLoading } = useGetProjectByIdQuery(id);
 
-  console.log(data);
+  console.log(data?.details);
 
   return isLoading ? (
     <LoaderSpinner />
   ) : (
-    <main className="flex flex-col gap-4 bg-white"></main>
+    <main className="flex flex-col gap-4 bg-white p-8 rounded-xl">
+      <header className="flex justify-between">
+        <section className="flex gap-12">
+          <div className="flex flex-col gap-2">
+            <Text>Project Owner</Text>
+            <Text size="small">{data?.details.ownerId.name}</Text>
+            <Text size="small">{data?.details.ownerId.email}</Text>
+            <Text size="small">{data?.details.ownerId.contact}</Text>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Text>Client </Text>
+            <Text size="small">{data?.details.acceptedClientId.name}</Text>
+            <Text size="small">{data?.details.acceptedClientId.email}</Text>
+            <Text size="small">{data?.details.acceptedClientId.contact}</Text>
+          </div>
+        </section>
+
+        <section className="flex flex-col gap-2">
+          <Text>Project Details</Text>
+
+          <div className="flex gap-4">
+            <Text size="small">Paying Status : </Text>
+            <Text size="small">{data?.details.salaryStatus}</Text>
+          </div>
+          <div className="flex gap-4">
+            <Text size="small">To Pay : </Text>
+            <Text size="small">
+              Rs. {data?.details.moneySentPerSalaryStatus}
+            </Text>
+          </div>
+          <div className="flex gap-4">
+            <Text size="small">Project Duration : </Text>
+            <Text size="small">{data?.details.projectDuration}</Text>
+          </div>
+        </section>
+      </header>
+    </main>
   );
 };
