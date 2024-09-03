90d99378c3546f567248b90bb2c90e1a75c9e1ac
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-03 10:39:12 +0545

    added

--- admin/src/pages/layout/layout.tsx
+++ admin/src/pages/layout/layout.tsx
@@ -20,7 +20,7 @@ export const Layout = () => {
         className={classnames(
           "flex ml-52 flex-col min-h-screen p-4 w-full gap-5 ",
           {
-            "ml-2": context?.sidebarStatus == false,
+            "ml-0": context?.sidebarStatus == false,
           }
         )}
       >
